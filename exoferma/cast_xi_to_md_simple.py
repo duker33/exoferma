67654084be2f7b01a927a6200c07079424944845
duker <duker33@gmail.com>
2021-10-20 12:22:04 +0300

    Simple cast to the doc format

--- exoferma/cast_xi_to_md_simple.py
+++ exoferma/cast_xi_to_md_simple.py
@@ -3,24 +3,36 @@ def text():
         return f.read()
 
 def cast(source: str) -> str:
-    return [cast_line(line) for line in source.split('\n')]
+    return '\n'.join([cast_line(line) for line in source.split('\n')])
 
 def cast_line(line: str) -> str:
-    if line_is_heading(line):
+    ll = line
+    if line_is_heading(ll):
+        ll = cast_heading(ll)
+    elif line_is_note(ll):
+        ll = cast_note(ll)
+    elif line_is_code(ll):
         pass
-    elif line_is_note(line):
-        pass
-    elif line_is_empty(line):
+    elif line_is_empty(ll):
         pass
     else:
         raise ValueError(f'Unknown line type:\n{line}')
+    return ll
 
 # <<
 def line_is_heading(line: str) -> bool:
-    pass
+    return (
+        line.strip().endswith(' .')
+        and not line_is_note(line)
+        and not line_is_code(line)
+    )
 
 def line_is_note(line: str) -> bool:
-    pass
+    ll = line.strip()
+    return ll == '.' or ll.startswith('. ')
+
+def line_is_code(line: str) -> bool:
+    return line.strip().startswith('| ')
 # >>
 
 def line_is_empty(line: str) -> bool:
@@ -31,13 +43,20 @@ def line_is_empty(line: str) -> bool:
 #     pass
 
 def cast_heading(line: str) -> str:
-    pass
+    assert line_is_heading(line)
+    indent = take_left_indent(line)
+    return f'{indent}**{line.strip().rstrip()[:-2]}**'
 
 def cast_note(line: str) -> str:
-    pass
+    assert line_is_note(line)
+    indent = take_left_indent(line)
+    return f'{indent}- {line.lstrip()[2:]}'.rstrip()
 
 def cast_link(chunk: str) -> str:
     pass
+
+def take_left_indent(line: str) -> str:
+    return line[:len(line)-len(line.lstrip())]
 # >>
 
 def main():
--- tests/test_cast_xi_to_md_simple.py
+++ tests/test_cast_xi_to_md_simple.py
@@ -1,33 +1,66 @@
-import exoferma as ef
+import pytest
+
+from exoferma import cast_xi_to_md_simple as cs
+
+ASSETS_BASE = 'tests/assets'
 
 
-def test_cast(source: str) -> str:
+def test_cast() -> str:
     pass
-
-def test_cast_line(line: str) -> str:
-    pass
+    with open(f'{ASSETS_BASE}/common_from.xi') as f:
+        l_from = f.read()
+    with open(f'{ASSETS_BASE}/common_to.xi') as f:
+        l_to = f.read()
+    assert cs.cast(l_from) == l_to
 
 # <<
-def test_line_is_heading(line: str) -> bool:
-    pass
+def test_line_is_heading() -> bool:
+    f = cs.line_is_heading
+    assert f('Heading .')
+    assert not f('. Note, but not Heading .')
+    assert f('  Heading .  ')
+    assert f('Heading . .')
+    assert not f('Not Heading')
+    assert not f('Not Heading too.')
+    assert not f('')
 
-def test_line_is_note(line: str) -> bool:
-    pass
+def test_line_is_note() -> bool:
+    f = cs.line_is_note
+    assert f('. Note')
+    assert f('  . Note')
+    assert f('. . Note')
+    assert f('. Note, but not Heading .')
+    assert not f('Not Note')
+    assert not f('Not Note too.')
+    assert not f('')
+    assert f('.')
+    assert f('  .')
+    assert f('.  ')
+    assert f('  .  ')
 # >>
 
-def test_line_is_empty(line: str) -> bool:
-    return not line.strip()
+def test_line_is_empty() -> bool:
+    pass
 
 # <<
-# def test_cast_(line: str) -> str:
+# def test_cast_() -> str:
 #     pass
 
-def test_cast_heading(line: str) -> str:
-    pass
+def test_cast_heading() -> str:
+    f = cs.cast_heading
+    assert f('One .') == '**One**'
+    with pytest.raises(AssertionError):
+        f('One')
+    with pytest.raises(AssertionError):
+        f('. One .')
 
-def test_cast_note(line: str) -> str:
-    pass
+def test_cast_note() -> str:
+    f = cs.cast_note
+    assert f('. One') == '- One'
+    with pytest.raises(AssertionError):
+        f('One')
+    assert f('. One .') == '- One .'
 
-def test_cast_link(chunk: str) -> str:
+def test_cast_link() -> str:
     pass
 # >>
